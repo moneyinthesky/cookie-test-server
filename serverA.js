bdeb057a0f36f32a12c66a9d49085908d065ec52
Barath Vignarajah <moneyinthesky@gmail.com>
2024-06-18 20:10:27 +0100

    SameSite none and secure

--- serverA.js
+++ serverA.js
@@ -12,7 +12,10 @@ app.get("/", (req, res) => {
 })
 
 app.get("/redirect", (req, res) => {
-	res.cookie("test", "value")
+	res.cookie("test", "value", {
+		sameSite: 'none',
+		secure: true
+	})
        .redirect(307, `https://cookie-test-server-b.onrender.com/`)
 })
 
